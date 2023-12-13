22e9efe5f2438a22a19e10fe384049f5760ad6d8
pavaberna <pavaberna@gmail.com>
2023-12-13 19:12:23 +0100

    server created

---
+++ public/script.js
@@ -0,0 +1,19 @@
+document.body.innerHTML= "lefutott a script !!"
+/* 
+import fs from "fs"
+const rawData = fs.readFileSync(__dirname + "/../public/database.json", "utf-8")
+const data = JSON.parse(rawData) */
+
+console.log("script arrived")
+
+/* const demo = async () => {
+    console.log("demo triggered")
+    
+    const response = await fetch("http://localhost:3000/data/all")
+
+    const data = await response.json()
+
+    console.log(data.countries)
+}
+
+demo() */
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log("hello world")
+import http, { IncomingMessage, ServerResponse } from "http"
+import fs from "fs/promises"
+
+const sleep = () => new Promise<void>((resolve) => setTimeout(() => resolve(), 3000))
+
+const requestListenerFunction = async (req: IncomingMessage, res: ServerResponse) => {
+
+    await sleep()
+
+    if (req.url === "/script.js") {
+        const script = await fs.readFile(`${__dirname}/../public/script.js`, "utf-8")
+        res.write(script)
+        res.end()
+        return
+    }
+
+    if (req.url === "/") {
+        const html = await fs.readFile(`${__dirname}/../public/index.html`, "utf-8")
+        res.write(html)
+        res.end()
+        return
+    }
+
+    if (req.url === "/data/all") {
+        const data = await fs.readFile(`${__dirname}/../public/database.json`, "utf-8")
+        res.write(data)
+        res.end()
+        return
+    }
+
+    if (req.url === "/data.js") {
+        const data = await fs.readFile(`${__dirname}/../public/database.json`, "utf-8")
+        const script = "const data = " + data + "; console.log(data)"
+        res.write(script)
+        res.end()
+        return
+    }
+
+    res.write("<h1>not found</h1>")
+    res.end()
+}
+
+http.createServer(requestListenerFunction).listen(3000)
